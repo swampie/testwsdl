a7ac81d69db8c5ea5637c7240101f6701b2c404a
Matteo Fiandesio <matteo.fiandesio@schibsted.com>
2018-11-27 17:05:21 +0100

    now async

--- src/main/java/com/test/wsdl/wsdlwrapper/web/DocumiAccountController.java
+++ src/main/java/com/test/wsdl/wsdlwrapper/web/DocumiAccountController.java
@@ -3,6 +3,7 @@ package com.test.wsdl.wsdlwrapper.web;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.test.wsdl.documi.account.AccountGetLoginLinkResponse;
 import com.test.wsdl.wsdlwrapper.services.AccountService;
+import org.reactivestreams.Subscription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.util.concurrent.Future;
 
 import static com.test.wsdl.wsdlwrapper.services.ReactiveAsyncHandler.into;
 
@@ -22,9 +22,15 @@ public class DocumiAccountController {
 
 
   @PostMapping("/documi/account/login")
-  public Mono<AccountGetLoginLinkResponse> getLogin(@RequestBody TokenHolder token) {
-    return Mono.create(sink -> accountService.getAsyncLoginResponse(token.getToken(), into(sink)));
+  public ResponseEntity<Void> getLogin(@RequestBody TokenHolder token) {
 
+    Mono<AccountGetLoginLinkResponse> result =  Mono.create(sink -> accountService.getAsyncLoginResponse(token.getToken(), into(sink)));
+    result.doOnError(e -> {
+      e.printStackTrace();
+    }).subscribe(accountGetLoginLinkResponse -> {
+      System.out.println(accountGetLoginLinkResponse.getAccountGetLoginLinkResult());
+    });
+    return ResponseEntity.accepted().build();
   }
 
   static class TokenHolder {
